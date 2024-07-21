278609fb93ece8ae957b5f6984cfeca2883870c9
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-07-21 20:42:21 +0900

    feat: Add process to output csv file.

--- src/goo_gl_archives/__init__.py
+++ src/goo_gl_archives/__init__.py
@@ -1,3 +1,4 @@
+from goo_gl_archives.utils.csv import write_to_csv
 from goo_gl_archives.utils.requests import generate_random_strings, get_redirect_info
 
 
@@ -13,10 +14,12 @@ class GooGlArchives:
     def __init__(self, count: int = 10):
         self.count = count
         self.base_url = "https://goo.gl/"
+        self.filename = "output.csv"
+        self.results = []
 
     def run(self) -> None:
         """
-        Main function to generate URLs, retrieve their redirect information.
+        Main function to generate URLs, retrieve their redirect information, and write the results to a CSV file.
         """
         # Generate unique strings
         unique_strings = generate_random_strings(self.count)
@@ -28,5 +31,19 @@ class GooGlArchives:
                 # Retrieve redirect information for the URL
                 redirect_info = get_redirect_info(full_url)
                 print(redirect_info)
+                self.results.append([uid] + list(redirect_info))
             except Exception as e:
                 print(f"Failed to get info for {full_url}: {e}")
+                self.results.append(
+                    [
+                        uid,
+                        full_url,
+                        None,
+                        None,
+                        None,
+                        None,
+                    ]
+                )
+
+        # Write results to a CSV file
+        write_to_csv(self.results, self.filename)
