dd665fbe7a12c858295a0fdf4313db0d3191be93
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-08-10 20:50:25 +0900

    feat: Add process to sanitize UTF-8.

--- generate-markdown/pkg/markdown/markdown.go
+++ generate-markdown/pkg/markdown/markdown.go
@@ -4,7 +4,27 @@ import (
 	"fmt"
 	"generate-markdown/internal/database"
 	"strings"
+	"unicode/utf8"
 )
+
+// sanitizeUTF8 ensures that a string is valid UTF-8.
+// If the string contains invalid UTF-8 sequences, it converts it to a valid UTF-8 string.
+func sanitizeUTF8(str string) string {
+	if !utf8.ValidString(str) {
+		return string([]rune(str))
+	}
+	return str
+}
+
+// escapeMarkdownSpecialChars escapes special characters in Markdown, such as pipes.
+func escapeMarkdownSpecialChars(str string) string {
+	str = strings.ReplaceAll(str, "|", "\\|")
+	str = strings.ReplaceAll(str, "`", "\\`")
+	str = strings.ReplaceAll(str, "[", "\\[")
+	str = strings.ReplaceAll(str, "]", "\\]")
+	str = strings.ReplaceAll(str, "!", "\\!")
+	return str
+}
 
 // GenerateTable generates a markdown table from a slice of database.Link structs.
 // Each row in the table corresponds to a link's data, formatted with specific columns.
@@ -27,12 +47,19 @@ func GenerateTable(links []database.Link) string {
 
 		// Check if PageTitle is not nil and use its value if available
 		if link.PageTitle != nil {
-			pageTitle = *link.PageTitle
+			// Ensure the page title is valid UTF-8 and escape special characters
+			pageTitle = sanitizeUTF8(*link.PageTitle)
+			pageTitle = escapeMarkdownSpecialChars(pageTitle)
 		}
+
+		// Escape other special characters in the link data
+		uid := escapeMarkdownSpecialChars(link.UID)
+		redirectURL := escapeMarkdownSpecialChars(link.RedirectURL)
+		domainName := escapeMarkdownSpecialChars(link.DomainName)
 
 		// Write a formatted row with the link data
 		sb.WriteString(fmt.Sprintf("| %d | `%s` | https://goo.gl/%s | ![%s](%s) | `%s` | `%s` | `%d` |\n",
-			i+1, link.UID, link.UID, link.RedirectURL, link.RedirectURL, link.DomainName, pageTitle, link.HTTPStatus))
+			i+1, uid, uid, redirectURL, redirectURL, domainName, pageTitle, link.HTTPStatus))
 	}
 
 	// Return the generated markdown table as a string
